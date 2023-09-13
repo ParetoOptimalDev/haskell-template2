1af2d88e75e00784493eb3e8c9975ece4fd3a970
ParetoOptimalDev <pareto.optimal@mailfence.com>
2023-09-13 09:10:38 -0500

    try specifying extra-substituter in cli args
    https://github.com/srid/nixci/issues/28#issuecomment-1716332508

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  nixConfig.binaryCaches = [
-    "http://127.0.0.1:37515"
-    "https://cache.nixos.org"
-  ];
-
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
